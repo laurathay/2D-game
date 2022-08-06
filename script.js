c696c9fc5985f37ede1f261f125fb55a34e79c81
Laura THAY <laura.thay@ipag.fr>
2022-08-07 01:39:16 +0200

    is it working?

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const canvas = getElementById('canvas1');
 const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+playerImage.src = 'shadow_dow.png';
