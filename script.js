5ccc45b1a3996e7f6a21aa92c52134b153f4a1e4
Laura THAY <laura.thay@ipag.fr>
2022-08-09 23:54:06 +0200

    little square

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
-playerImage.src = 'shadow_dow.png';
+playerImage.src = 'shadow_dog.png';
+
+function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.fillRect(100,50,100,100);
+    ctx.drawImage(playerImage, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    requestAnimationFrame(animate);
+};
+animate();
