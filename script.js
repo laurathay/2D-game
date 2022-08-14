61353a9e4faa688682554c4be758e6339cb4e388
Laura THAY <laura.thay@ipag.fr>
2022-08-14 15:50:57 +0200

    the bug is in there

--- script.js
+++ script.js
@@ -6,10 +6,17 @@ const CANVAS_HEIGHT = canvas.height = 600;
 const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
 
+const spriteWidth = 575;
+const spriteHeight = 523;
+
+let frameX = 1;
+let frameY = 5;
+
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.fillRect(100,50,100,100);
-    ctx.drawImage(playerImage, 0, 0, 800, 800, 0, 0,CANVAS_WIDTH, CANVAS_HEIGHT);
+    //ctx.fillRect(100,50,100,100);
+    //ctx.drawImage(playerImage, 0, 0, 800, 800, 0, 0,CANVAS_WIDTH, CANVAS_HEIGHT);
+   ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
     requestAnimationFrame(animate);
 };
 animate();
