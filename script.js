5b6859e3382604e0cb3be3763aa5d3b73d1a1251
Laura THAY <laura.thay@ipag.fr>
2022-08-15 17:05:36 +0200

    ajout des commentaires

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
+//tu créer ton cadre 
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
+//sa taile 
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
+//tu ajoutes l'image qui va dedans
 const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
 
+//en lui mettant sa dimension
 const spriteWidth = 575;
 const spriteHeight = 523;
 
+//tu parcours le dessin par frame en horizontal
 let gameFrame = 0;
 const staggerFrames = 5;
 
+//tu créer le tableau avec les differents etats
 const spriteAnimations = [];
 const animationStates = [
     {
@@ -55,10 +61,12 @@ const animationStates = [
         frames: 4,
     }
 ];
+//letat selon lindex
 animationStates.forEach((state, index) => {
     let frames = {
         loc: [],
     }
+    //on parcours le tableau selon le nombre de frames loc
     for (let j=0; j < state.frames; j++){
         let positionX = j * spriteWidth;
         let positionY = index * spriteHeight;
@@ -68,11 +76,13 @@ animationStates.forEach((state, index) => {
     spriteAnimations[state.name] = frames;
 });
 
+//on créer le cadre noir puis on regle la vistesse avec math.floor et cela bouge avec la loc d'en haut 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations["idle"].loc.length;
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations["ko"].loc.length;
     let frameX = spriteWidth * position;
-    let frameY = spriteAnimations["idle"].loc[position].y;
+    let frameY = spriteAnimations["ko"].loc[position].y;
+    //le dessine dans le cadre attitre
     ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
     
     gameFrame++;
