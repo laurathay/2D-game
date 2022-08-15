bb577b5b83030a0b00f750a0f5995e2815ea597a
Laura THAY <laura.thay@ipag.fr>
2022-08-15 16:50:38 +0200

    unbugged document. missing l1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const canvas = getElementById('canvas1');
+const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
@@ -12,11 +12,40 @@ const spriteHeight = 523;
 let frameX = 2;
 let frameY = 5;
 
+let gameFrame = 0;
+const staggerFrames = 5;
+
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name:'idle',
+        frames: 7,
+    },
+    {
+        name:'jump',
+        frames: 7,
+    }
+];
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
+    }
+    for (let j=0; j < state.frames; j++){
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({x: positionX, y:positionY});
+    }
+    //pour parcourir le tableau d'animations
+    spriteAnimations[state.name] = frames;
+});
+
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    //ctx.fillRect(100,50,100,100);
-    //ctx.drawImage(playerImage, 0, 0, 800, 800, 0, 0,CANVAS_WIDTH, CANVAS_HEIGHT);
-   ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+    let position = Math.floor(gameFrame/staggerFrames) % 6;
+    frameX = spriteWidth * position;
+    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+    
+    gameFrame++;
     requestAnimationFrame(animate);
 };
 animate();
