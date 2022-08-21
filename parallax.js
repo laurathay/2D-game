933a7a8b1c9cd938814a12878b537546b7cfa975
Laura THAY <laura.thay@ipag.fr>
2022-08-21 17:04:38 +0200

    [add] animate function

--- parallax.js
+++ parallax.js
@@ -16,3 +16,18 @@ backgroundLayer4.src = 'layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'layer-5.png';
 
+let x = 0;
+let x2 = 2400;
+
+function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.drawImage(backgroundLayer4, x, 0);
+    ctx.drawImage(backgroundLayer4, x2, 0);
+    if (x < -2400) x = 2400 + x2 - gameSpeed;
+    else x -= gameSpeed;
+    if (x2 < -2400) x2 = 2400+ gameSpeed;
+    else x2 -= gameSpeed;
+    requestAnimationFrame(animate); 
+};
+
+animate();
