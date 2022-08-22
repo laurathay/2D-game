983de81861f2d79c32f925d7b311211bd64d203b
Laura THAY <laura.thay@ipag.fr>
2022-08-22 21:46:44 +0200

    on update et draw dans la fonction methode

--- parallax.js
+++ parallax.js
@@ -16,8 +16,10 @@ backgroundLayer4.src = 'layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'layer-5.png';
 
+//la class possède les propriétés
 class Layer {
     constructor(image, speedModifier){
+        //on initialise
         this.x = 0;
         this.y = 0;
         this.width = 2400;
@@ -28,18 +30,33 @@ class Layer {
         this.speed = gameSpeed * this.speedModifier;
     }
     update(){
+        //on update le speed
         this.speed = gameSpeed * this.speedModifier;
         if (this.x <= -this.width){
-            this.x = this.width + this.x - this.speed;
+            this.x = this.width + this.x2 - this.speed;
         }
+        if (this.x2 <= -this.width){
+            this.x2 = this.width + this.x - this.speed;
+        }
+        this.x = Math.floor(this.x - this.speed);
+        this.x2 = Math.floor(this.x2 - this.speed);
     }
     draw() {
-        
+        //on le dessine selon les coordonnées
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
     }
 }
 
+//on l'appelle ici pour qu'elle prenne en compte le layer qu'on veut
+const layer4 = new Layer(backgroundLayer4, 0.5);
+
+//la methode etant la fonction
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    //on l'appelle ici dans la fonction
+    layer4.update();
+    layer4.draw();
     requestAnimationFrame(animate); 
 };
 
