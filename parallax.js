f6b53784becee7db9de1adc97c80115b5c27d4d8
Laura THAY <laura.thay@ipag.fr>
2022-08-25 19:33:01 +0200

    create object to fit all layer

--- parallax.js
+++ parallax.js
@@ -55,14 +55,16 @@ const layer3 = new Layer(backgroundLayer3, 0.5);
 const layer4 = new Layer(backgroundLayer4, 0.5);
 const layer5 = new Layer(backgroundLayer5, 1);
 
+const gameObjects = [layer1, layer2, layer3, layer4, layer5]
+
 //la methode etant la fonction
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     //on l'appelle ici dans la fonction
-    layer4.update();
-    layer4.draw();
-    layer5.update();
-    layer5.draw();
+    gameObjects.forEach(object => {
+        object.update();
+        object.draw();
+    });
     requestAnimationFrame(animate); 
 };
 
